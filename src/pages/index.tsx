c71760d632832e5452d4c7be6dd9a90b61b9935d
Mikhaella <76699403+MikhaellaThet@users.noreply.github.com>
2021-02-23 13:28:43 -0300

    A lot of Components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
+import React from "react";
+import { CompletedChallengers } from "../components/CompletedChallengers";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import Head from 'next/head'
+
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio</title>
+      </Head>
     <ExperienceBar/>
+    <section>
+      <div>
+        <Profile />
+        <CompletedChallengers />
+        <Countdown />
+      </div>
+      <div>
+
+      </div>
+    </section>
   </div>   
   )
 }
